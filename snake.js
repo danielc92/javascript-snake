4ac07dc65910a2562bd3dcf2668db0220e542749
Daniel Corcoran <danielc.92@outlook.com>
2019-06-03 09:42:40 +1000

    Initial setup/draw functions and global variables

---
+++ snake.js
@@ -0,0 +1,29 @@
+// Setup global variables
+let w = 1000;
+let h = w;
+let offset = 50;
+let points = [];
+
+
+// Initial setup function
+function setup () {
+    // Create the canvas as a square
+    createCanvas(w, h);
+   
+    // Set frame rate
+    frameRate(14);
+
+    // Initialize instance of food and snake
+
+
+}
+
+
+// Draw function loops indefinitely
+function draw () {
+    // Colour background black
+    background(0);
+
+}
+
+
