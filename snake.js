7753ecc599af098829da56f4f910c00a70ef16f3
Daniel Corcoran <danielc.92@outlook.com>
2019-06-03 09:50:46 +1000

    Added code to generate points

--- snake.js
+++ snake.js
@@ -1,8 +1,22 @@
-// Setup global variables
+/* 
+Global variables setup
+The arena is to be square, so height will equal width
+offset is the size of each square, for now let it be 20
+*/
 let w = 1000;
 let h = w;
-let offset = 50;
+let offset = 20;
 let points = [];
+
+
+// The possible locations of all points
+// Used to generate new coordinates for Snake/Food instances
+for (let i = 0; i <= w - offset; i += offset) {
+    points.push(i);
+}
+
+
+
 
 
 // Initial setup function
